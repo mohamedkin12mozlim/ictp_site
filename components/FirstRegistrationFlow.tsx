6bf3a85f1aa9ef1548fccb17e452e2a044b51314
mohamedkin12mozlim <mohm10257@gmail.com>
2026-06-05 07:24:59 +0300

    finish1

--- components/FirstRegistrationFlow.tsx
+++ components/FirstRegistrationFlow.tsx
@@ -246,24 +246,23 @@ const verifyEmailOTP = async () => {
 
 //--------------------------------
 const saveToSupabase = async () => {
-const { data, error } = await supabase
-  .from('registrations')
-  .upsert(
-    [
-      {
-        full_name_ar: formData.nameAr,
-        full_name_en: formData.nameEn,
-        national_id: formData.nationalId,
-        phone: formData.whatsapp,
-        email: formData.email,
-        college: formData.faculty
-      }
-    ],
-    {
-      onConflict: 'email'
-    }
-  )
-  .select();
+
+  try {
+
+    const { data, error } = await supabase
+      .from('registrations')
+      .insert([
+        {
+          full_name_ar: formData.nameAr,
+          full_name_en: formData.nameEn,
+          national_id: formData.nationalId,
+          phone: formData.whatsapp,
+          email: formData.email,
+          college: formData.faculty
+        }
+      ])
+      .select();
+
     console.log("DATA:", data);
     console.log("ERROR:", error);
 
@@ -277,6 +276,7 @@ const { data, error } = await supabase
   } catch (err) {
 
     console.log("CATCH ERROR:", err);
+
     alert("حصل خطأ");
 
     return false;
