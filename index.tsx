128bc3e4da4d4b7b0c9c9050c57ea3697087bde0
mohamedkin12mozlim <mohm10257@gmail.com>
2026-06-07 08:00:37 +0300

    finish all3

--- index.tsx
+++ index.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import { LanguageProvider } from './LanguageContext';
 import { ThemeProvider } from './ThemeContext';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights"
 
 
 const rootElement = document.getElementById('root');
